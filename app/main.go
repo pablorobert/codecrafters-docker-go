fa4d388d0e77ddad5e700357a2f24b15ac257d45
Pablo Alves <pablorobert@gmail.com>
2024-05-19 12:18:18 -0300

    ls

--- app/main.go
+++ app/main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"fmt"
 	"os"
+	"syscall"
 	"strconv"
 	"os/exec"
 )
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 func main() {
+
 	command := os.Args[3]
 	args := os.Args[4:len(os.Args)]
 	
@@ -19,19 +21,35 @@ func main() {
 		args[0] = "echo"
 		isError = true
 	}
-	if (args[0] == "exit") {
+	if args[0] == "exit" {
 		exitCode, err = strconv.Atoi(args[1])
 		if (err != nil) {
 			exitCode = 0
 		}
 		os.Exit(exitCode)
 	}
-	
+	if args[0] == "ls" {
+		err := syscall.Mkdir(args[1] + "/dev", 0777)
+		if (err != nil) {
+			fmt.Println("erro ao criar diretorio1")
+			fmt.Printf("%v", err)
+		}
+		err = syscall.Mkdir(args[1] + "/dev/null", 0777)
+		if (err != nil) {
+			fmt.Println("erro ao criar diretorio2")
+			fmt.Printf("%v", err)
+		}
+		syscall.Chroot(args[1])
+		syscall.Chdir("/")
+	}
+
 	cmd := exec.Command(command, args...)
+	//cmd.Stdin = os.Stdin
+	//cmd.Stderr = os.Stderr
 	output, err := cmd.Output()
 	if err != nil {
-		fmt.Printf("Err: %v", err)
-		os.Exit(1)
+		fmt.Print("No such file or directory")
+		os.Exit(2)
 	}
 
 	if (isError) {
