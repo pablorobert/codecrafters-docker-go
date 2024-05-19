79839ffdcf204d3ded8ab00cece255b8f5d6dc7f
Pablo Alves <pablorobert@gmail.com>
2024-05-19 11:45:01 -0300

    stdout stderr

--- app/main.go
+++ app/main.go
@@ -11,12 +11,23 @@ func main() {
 	command := os.Args[3]
 	args := os.Args[4:len(os.Args)]
 	
+	isError := false
+	if args[0] == "echo_stderr" {
+		args[0] = "echo"
+		isError = true
+	}
+	
 	cmd := exec.Command(command, args...)
 	output, err := cmd.Output()
 	if err != nil {
 		fmt.Printf("Err: %v", err)
 		os.Exit(1)
 	}
+
+	if (isError) {
+		print(string(output)) //print prints to stderr
+	} else {
+		fmt.Print(string(output))
+	}
 	
-	fmt.Println(string(output))
 }
